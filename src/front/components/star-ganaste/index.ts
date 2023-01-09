d88d2b5e207b6d58878eaff4cc29fc2f3d765eaf
carlosgovi <cgovi66@gmail.com>
2023-01-09 01:29:21 -0300

    cambio en el package saco ghpages dependencias

--- src/front/components/star-ganaste/index.ts
+++ src/front/components/star-ganaste/index.ts
@@ -9,7 +9,8 @@ export function initStarGanasteComp() {
       }
       render() {
         const img: any = document.createElement("img");
-        const imagenSrc = require("/src/front/img/resultado-ganaste.svg");
+        const imagenSrc = require(__dirname +
+          "/src/front/img/resultado-ganaste.svg");
 
         img.src = imagenSrc;
         this.shadow.appendChild(img);
--- src/front/components/star-perdiste/index.ts
+++ src/front/components/star-perdiste/index.ts
@@ -9,7 +9,8 @@ export function initStarPerdisteComp() {
       }
       render() {
         const img: any = document.createElement("img");
-        const imagenSrc = require("/src/front/img/resultado-perdiste.svg");
+        const imagenSrc = require(__dirname +
+          "/src/front/img/resultado-perdiste.svg");
 
         img.src = imagenSrc;
         this.shadow.appendChild(img);
